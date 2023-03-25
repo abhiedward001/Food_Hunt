3ce3623fbcd3fda67068540fb5054f18b12d9ba3
abhiedward001 <iam001abhi@gmail.com>
2023-03-25 20:18:05 +0530

    added HeaderCartButton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,12 @@
 import styles from './Header.module.css';
 import mealImage from '../../assets/meals.jpeg';
-
+import HeaderCartButton from './HeaderCartButton';
 const Header = () => {
     return (
         <>
             <header className={styles.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCartButton></HeaderCartButton>
             </header>
             <div className={styles['main-image']}>
                 <img src={mealImage} alt='This Image is background Image which is not rendering right now'></img>
