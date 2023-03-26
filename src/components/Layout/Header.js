3b1bb1803108e6a59a5576d650245fd8205b39fb
abhiedward001 <iam001abhi@gmail.com>
2023-03-26 16:21:37 +0530

    added the Card wrapper for Meals Items and Added the Meal components

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,18 @@
 import styles from './Header.module.css';
 import mealImage from '../../assets/meals.jpeg';
 import HeaderCartButton from './HeaderCartButton';
+
+// Component for Header
+
 const Header = () => {
     return (
         <>
             <header className={styles.header}>
                 <h1>ReactMeals</h1>
+                
+                {/* This is a header button component */}
                 <HeaderCartButton></HeaderCartButton>
+
             </header>
             <div className={styles['main-image']}>
                 <img src={mealImage} alt='This Image is background Image which is not rendering right now'></img>
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,19 @@
 import CartIcon from "../Cart/CartIcon";
 import styles from './HeaderCartButton.module.css';
+
+//Component for the header button
+
 const HeaderCartButton = () => {
     return (
         <>
             <button className={styles.button}>
                 <span className={styles.icon}>
+
+                    {/* This is a component which containts svg file content for cart icon */}
                     <CartIcon></CartIcon>
                 </span>
                 <span >
-                    Your Cart 
+                    Your Cart
                 </span>
                 <span className={styles.badge}>
                     3
